3aa3a9da1d190fa06b32ba2b1049cd060936aae7
Vu Quoc Ngoc <vuquocngoc1989@gmail.com>
2022-10-12 06:37:21 +0700

    unit 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                My first Component
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
